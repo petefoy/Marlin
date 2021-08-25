450f2303906a1a947d84c82c6300d99e4e349ad2
petefoy <5297089+petefoy@users.noreply.github.com>
2021-08-24 20:32:44 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -987,11 +987,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 25, 25, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 25, 25, -3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 25
+#define PROBING_MARGIN 15
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (133*60)
@@ -1127,8 +1127,8 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -4
-#define Y_MIN_POS -32 //-52
+#define X_MIN_POS -13
+#define Y_MIN_POS -18 
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
