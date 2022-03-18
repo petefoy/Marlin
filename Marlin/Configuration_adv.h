fb9f1eea0a39ec66f10882e08680fb3321c0e272
petefoy <5297089+petefoy@users.noreply.github.com>
2022-03-17 19:53:10 -0500

    accel and lin adv
    acceleration and linear advance changes

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1907,12 +1907,12 @@
  *
  * See https://marlinfw.org/docs/features/lin_advance.html for full instructions.
  */
-//#define LIN_ADVANCE //pf
+#define LIN_ADVANCE //pf
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define LIN_ADVANCE_K 0.22    // Unit: mm compression per 1mm/s extruder speed
+  #define LIN_ADVANCE_K 0.6  // pf 0.22    // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
-  //#define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration
+  #define EXPERIMENTAL_SCURVE //pf // Enable this option to permit S-Curve Acceleration
 #endif
 
 // @section leveling
